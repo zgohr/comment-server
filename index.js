d8a240fd963803e9a5886cf79c1808daa2d431ab
Zachary Gohr <zachary.gohr@gmail.com>
2013-06-01 14:12:46 -0500

    Swtich from Socket.IO to SockJS.

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
-var io  = require('socket.io').listen(8080);
+var http = require('http');
+var sock = require('sockjs').createServer();
 
 
-io.sockets.on('connection', function(socket) {
-  socket.on('comment', function(data) {
-    console.log('New comment: ' + data.serialized);
-    socket.broadcast.emit('add-comment', data);
+var clients = {};
+
+sock.on('connection', function(conn) {
+  clients[conn.id] = conn;
+
+  conn.on('data', function(message) {
+    console.log(message);
+    for (key in clients) {
+      if (clients.hasOwnProperty(key)) {
+        clients[key].write(message);
+      }
+    };
   });
 });
+
+var server = http.createServer();
+
+sock.installHandlers(server, {prefix: '/sock'});
+server.listen(9999, '0.0.0.0');
